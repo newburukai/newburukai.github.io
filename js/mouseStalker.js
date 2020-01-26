109dee6fe397a190b199ada60e92b82be78dd8fe
にゅ〜ぶる <newburu@gmail.com>
2020-01-26 10:13:15 +0900

    画像のアニメーション処理をJavaScriptで実装

--- js/mouseStalker.js
+++ js/mouseStalker.js
@@ -7,4 +7,14 @@ window.onload = function () {
         stalker.style.left = e.clientX + 20 + "px";
         stalker.style.top = e.clientY + 20 + "px";
     });
+
+    let count = 1;
+    const imageChange = function () {
+        stalker.setAttribute('src', "./img/mouse/" + count + ".gif");
+        count++;
+        if (count > 9) {
+            count = 1;
+        }
+    } 
+    setInterval(imageChange, 100);
 };
